2d222078d2eadb4265d948c7d500cd4c8df21ff2
sakeyna <sakeyna.aaziz@gmail.com>
2024-10-25 12:45:44 -0500

    create Task model

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=225, default="Untitled")
+    description = models.TextField(blank=True, null=True)
+    completed = models.BooleanField(default=False)
+    due_date = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
