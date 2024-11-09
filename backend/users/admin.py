c583beff8e51fd338ed667c95fac97f092f9d4d1
sakeyna <sakeyna.aaziz@gmail.com>
2024-11-09 04:31:30 -0600

    register new models in admin.py

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile, Component, Tag
 
 # Register your models here.
 admin.site.register(Profile)
+admin.site.register(Component)
+admin.site.register(Tag)
