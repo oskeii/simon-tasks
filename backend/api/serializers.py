d6a32851fc3b651f4f2aa27be0d6697af7973f0a
sakeyna <sakeyna.aaziz@gmail.com>
2025-02-09 12:42:32 -0600

    fix #13: ProfileSerializer can properly re-structure request data for updates, and filter out empty fields.

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
 from tasks.models import Task
 from users.models import Profile
+from django.http.request import QueryDict
 
 
 class TaskSerializer(serializers.ModelSerializer):
@@ -75,20 +76,46 @@ class ProfileSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Profile
-        # fields = '__all__'
         fields = [
             'id',
             'user',
             'image',
             # Add other Profile fields here
             ]
-        
+    
+
+    def to_internal_value(self, data):
+        # Make a mutable copy of the data if it's a QueryDict
+        if isinstance(data, QueryDict):
+            print("THIS IS QUERYDICT")
+            data = data.copy()
+            print(data)
+
+        # Handle flat data and nest it under 'user'
+        user_fields = ['username', 'email', 'first_name', 'last_name']
+        # user_data = {field: data.pop(field)[0] for field in user_fields if field in data}
+        user_data = {}
+        for field in user_fields:
+            if field in data:
+                # print(field, data[field])
+                if data[field]:
+                    user_data[field] = data.pop(field)[0]
+                else:
+                    data.pop(field)
+                # print('\nuser-data:', user_data)
+                # print(data)
+        print(user_data)
+        data['user'] = user_data
+        print(data)
+        return data
     
     def update(self, instance, validated_data):
         # pop the nested user data to be handled separately
         print("Validated data:", validated_data)
-        user_data = validated_data.pop('user')
-        print(user_data)
+        validated_data = {field: data[0] for (field, data) in validated_data.items() if isinstance(data, list)}
+        print("Reformat validated data:", validated_data)
+        user_data = validated_data.pop('user', None)
+        print(user_data, type(user_data))
 
         # Update profile fields
         for attr, value in validated_data.items():
